b7414890223d0427c795f34713bdc7a33bb39441
aspirez4 <nikii.ivanov@gmail.com>
2016-03-10 14:40:45 +0200

    Bug fixes

--- App.jsx
+++ App.jsx
@@ -1,30 +1,33 @@
+// App component - represents the whole app
 App = React.createClass({
 
+    // This mixin makes the getMeteorData method work
     mixins: [ReactMeteorData],
 
     getInitialState() {
-      return {
-          hideCompleted: false
-      }
+        return {
+            hideCompleted: false
+        }
     },
 
     // Loads items from the Tasks collection and puts them on this.data.tasks
     getMeteorData() {
         let query = {};
 
-        if(this.state.hideCompleted) {
+        if (this.state.hideCompleted) {
             // If hide completed is checked, filter tasks
-            query = {checked: {$ne: true}}
+            query = {checked: {$ne: true}};
         }
 
         return {
-            tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch(),
+            tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
             incompleteCount: Tasks.find({checked: {$ne: true}}).count(),
             currentUser: Meteor.user()
-        }
+        };
     },
 
     renderTasks() {
+        // Get tasks from this.data.tasks
         return this.data.tasks.map((task) => {
             const currentUserId = this.data.currentUser && this.data.currentUser._id;
             const showPrivateButton = task.owner === currentUserId;
@@ -39,18 +42,18 @@ App = React.createClass({
     handleSubmit(event) {
         event.preventDefault();
 
-        //Find the text field via the React ref
-        var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+        // Find the text field via the React ref
+        var text = React.findDOMNode(this.refs.textInput).value.trim();
 
-        Meteor.call('addTask', text);
+        Meteor.call("addTask", text);
 
         // Clear form
-        ReactDOM.findDOMNode(this.refs.textInput).value = "";
+        React.findDOMNode(this.refs.textInput).value = "";
     },
 
     toggleHideCompleted() {
         this.setState({
-           hideCompleted: ! this.state.hideCompleted
+            hideCompleted: ! this.state.hideCompleted
         });
     },
 
@@ -71,15 +74,13 @@ App = React.createClass({
 
                     <AccountsUIWrapper />
 
-                    {
-                        this.data.currentUser ?
-                            <form className="new-task"
-                                  onSubmit={this.handleSubmit}>
-                                <input
-                                    type="text"
-                                    ref="textInput"
-                                    placeholder="Type to add new tasks" />
-                            </form> : ''
+                    { this.data.currentUser ?
+                        <form className="new-task" onSubmit={this.handleSubmit} >
+                            <input
+                                type="text"
+                                ref="textInput"
+                                placeholder="Type to add new tasks" />
+                        </form> : ''
                     }
                 </header>
 
--- todos-application-react.jsx
+++ todos-application-react.jsx
@@ -1,22 +1,23 @@
 // Define a collection to hold our tasks
-Tasks = new Mongo.Collection('tasks');
+Tasks = new Mongo.Collection("tasks");
 
-if(Meteor.isClient) {
+if (Meteor.isClient) {
     // This code is executed on the client only
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_ONLY"
     });
 
-    Meteor.subscribe('tasks');
+    Meteor.subscribe("tasks");
 
-    Meteor.startup(() => {
-        ReactDOM.render(<App />, document.getElementById('render-target'));
+    Meteor.startup(function () {
+        // Use Meteor.startup to render the component after the page is ready
+        React.render(<App />, document.getElementById("render-target"));
     });
 }
 
-if(Meteor.isServer) {
+if (Meteor.isServer) {
     // Only publish tasks that are public or belong to the current user
-    Meteor.publish('tasks', function () {
+    Meteor.publish("tasks", function () {
         return Tasks.find({
             $or: [
                 { private: {$ne: true} },
@@ -29,8 +30,7 @@ if(Meteor.isServer) {
 Meteor.methods({
     addTask(text) {
         // Make sure the user is logged in before inserting a task
-
-        if( !Meteor.userId()) {
+        if (! Meteor.userId()) {
             throw new Meteor.Error("not-authorized");
         }
 
@@ -59,15 +59,15 @@ Meteor.methods({
             throw new Meteor.Error("not-authorized");
         }
 
-        Tasks.update(taskId, { $set: { checked: setChecked } });
+        Tasks.update(taskId, { $set: { checked: setChecked} });
     },
 
     setPrivate(taskId, setToPrivate) {
         const task = Tasks.findOne(taskId);
 
         // Make sure only the task owner can make a task private
-        if(task.owner !== Meteor.userId()) {
-            throw new Meteor.Error("not-authorized")
+        if (task.owner !== Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
         }
 
         Tasks.update(taskId, { $set: { private: setToPrivate } });
